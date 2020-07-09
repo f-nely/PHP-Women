4e5350bf08c6eaff492012bf170cc5f0599d9d18
nely de faria <nelly.nf54@gmail.com>
2020-07-09 20:17:06 -0300

    Adiciona arrays.php

---
+++ PHP_Women/arrays.php
@@ -0,0 +1,15 @@
+<?php
+
+/*$myFruit = ["apple", "orange", "banana"];
+var_dump($myFruit);
+
+foreach ($myFruit as $value) {
+  echo $value . PHP_EOL;
+}*/
+
+for ($i=0; $i <= 10 ; $i++) { 
+  $a[] = $i * 10;
+}
+foreach ($a as $value) {
+  echo $value . PHP_EOL;
+}
